e431157208d180d932b5d2c8a6ef6e4ac287c106
Danielle Morgan <daamorgan@hotmail.com>
2020-04-14 20:43:08 -0500

    Database uri updated

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ UPLOAD_FOLDER='./app/static/uploads'
 app=Flask(__name__)
 app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
 app.config['SECRET_KEY']="Xz9am$hu7gry"
-app.config['SQLALCHEMY_DATABASE_URI']=DATABASE_URL="postgres://project1:password123@localhost/project1" 
+app.config['SQLALCHEMY_DATABASE_URI']=DATABASE_URL='postgresql://xpeuypmwyhyqsm:728951833892fd09ed749c2f690ce5e019babf1562029251c67041df607ad8ed@ec2-18-210-51-239.compute-1.amazonaws.com:5432/db2a5hquhv9oot' #"postgres://project1:password123@localhost/project1" 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db=SQLAlchemy(app)
 
